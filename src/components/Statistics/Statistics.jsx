2ac7b8db2fa58657e71e326c7544b2e9965ef945
maodzhedun <48294837+maodzhedun@users.noreply.github.com>
2023-09-24 14:33:04 +0300

    2.2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,54 +7,61 @@ class Statistics extends Component {
     bad: 0,
   };
 
-  incrementGood = () => {
+  // Обработчик клика на кнопке, который увеличивает значение соответствующей категории обратной связи.
+  onclickButton = key => {
     this.setState(prevState => ({
-      good: prevState.good + 1,
+      [key]: prevState[key] + 1,
     }));
   };
 
-  incrementNeutral = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1,
-    }));
-  };
-
-  incrementBad = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
-  };
-
+  // Функция для подсчета общего количества полученной обратной связи.
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage() {}
+  // Функция для подсчета процента положительной обратной связи. (Должна быть реализована).
+  countPositiveFeedbackPercentage() {
+    const totalCount = this.countTotalFeedback();
+    // Здесь нужно реализовать логику подсчета процента положительной обратной связи.
+     return  totalCount === 0 ? 0 : Math.round((this.state.good / this.countTotalFeedback()) * 100)
+    
+  }
 
-  render() {
+  render()
+  {
+   
+    const keys = Object.keys(this.state);
+    const totalCount = this.countTotalFeedback()
+    console.log(totalCount);
     return (
       <div>
         <h1>Please leave feedback</h1>
         <div>
-          <button type="button" onClick={this.incrementGood}>
-            Good
-          </button>
-          <button type="button" onClick={this.incrementNeutral}>
-            Neutral
-          </button>
-          <button type="button" onClick={this.incrementBad}>
-            Bad
-          </button>
+          {keys.map(key => (
+            // Кнопка для каждой категории обратной связи с обработчиком клика.
+            <button
+              type="button"
+              key={key}
+              onClick={() => this.onclickButton(key)}
+            >
+              {key}
+            </button>
+          ))}
         </div>
         <h2>Statistics</h2>
-        <div>
-          <p>Good: {this.state.good}</p>
-          <p>Neutral: {this.state.neutral}</p>
-          <p>Bad: {this.state.bad}</p>
-          <p>Total: </p>
-          <p>Positive feedback: </p>
-        </div>
+        {totalCount === 0 ? (
+          <h2>Get feedabck</h2>
+        ) : (
+          <div>
+            <p>Good: {this.state.good}</p>
+            <p>Neutral: {this.state.neutral}</p>
+            <p>Bad: {this.state.bad}</p>
+            <p>Total: {totalCount}</p>
+            {/* Здесь должно отобразиться значение положительной обратной связи */}
+            <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+          </div>
+        )}
       </div>
     );
   }
